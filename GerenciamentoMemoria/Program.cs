3d61603682ef99114d3c05debb0812a43af05393
Rubens Lyra <rubensvlyra@gmail.com>
2022-12-04 12:08:20 -0300

    iniciando projeto de codigo

--- GerenciamentoMemoria/Program.cs
+++ GerenciamentoMemoria/Program.cs
@@ -6,7 +6,22 @@ namespace GerenciamentoMemoria
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Gerenciamento de Memória - C#");
+        }
+
+        public struct Endereco
+        {
+            public string Rua;
+            public string Cidade;
+            public string Estado;
+            public string CEP;
+
+            // Nesse caso, a variável Endereco é um tipo de valor, e não um tipo de referência.
+        }
+
+        static void Teste()
+        {
+            int[] numeros = new int[100000000];
         }
     }
 }
